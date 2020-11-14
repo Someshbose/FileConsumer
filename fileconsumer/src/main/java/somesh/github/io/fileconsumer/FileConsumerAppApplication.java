f889c999f72afdefef99f8af7fcab72af8adbfcb
Someshbose <iamsomeshbose@gmail.com>
2020-11-14 10:38:47 +0530

    Initial commit

---
+++ fileconsumer/src/main/java/somesh/github/io/fileconsumer/FileConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package somesh.github.io.fileconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileConsumerAppApplication.class, args);
+	}
+
+}
---
+++ fileconsumer/src/test/java/somesh/github/io/fileconsumer/FileConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package somesh.github.io.fileconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConsumerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
